8062aa6856e4791db21ceec578eb278b397c715a
Juan Crispi <jccrispi@gmail.com>
2015-09-01 17:41:54 -0400

    class

---
+++ scriptturninHTML/turninHTML.sh
@@ -0,0 +1,13 @@
+#!ceholder/bin/bash
+
+cat index.html > modified.html
+
+echo -n "Type the command:  "
+read command
+
+echo  `$command` >  temp.html
+
+sed  "s/CofC/$(cat temp.html)/" modified.html > CSCI362.html
+
+echo  `gnome-open CSCI362.html`
+
